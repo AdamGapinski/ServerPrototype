9c45c071864771fe6fcd625bdbfcda43a2e2536f
Adam Gapinski <gapinski.adam1@gmail.com>
2017-12-04 13:54:04 +0100

    ServerPrototypeApplication main class

---
+++ src/main/java/com/prototype/serverprototype/ServerPrototypeApplication.java
@@ -0,0 +1,12 @@
+package com.prototype.serverprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerPrototypeApplication.class, args);
+	}
+}
